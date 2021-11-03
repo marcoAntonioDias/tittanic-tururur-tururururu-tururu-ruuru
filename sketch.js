4449ccd95b8717d125eb627ac1422700cb209c10
marcoAntonioDias <79108396+marcoAntonioDias@users.noreply.github.com>
2021-11-03 15:48:34 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var oceano;
+var titanic;
+function preload(){
+oceano = loadImage("sea.png");
+titanic = loadAnimation("ship-1.png,ship-2.png,ship-3.png,ship-4.png");
+oceano.velocityX=4;
+}
+
+function setup(){
+  createCanvas(400,400);
+if (oceano.x< 0){;
+  oceano.x = oceano.width/2;
+}
+}
+
+function draw() {
+  background("blue");
+    drawSprites();
+
+ 
+}
